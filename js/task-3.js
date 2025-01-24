32e94081970426f041859a28d98a45841902b9ff
Dimqe <petr.makarov249@gmail.com>
2025-01-24 14:23:19 +0100

    dom

--- js/task-3.js
+++ js/task-3.js
@@ -6,6 +6,9 @@ function checkForSpam(message) {
       return false;
     }
   }
+
+
+
   console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
 console.log(checkForSpam("Get best sale offers now!")); // true
